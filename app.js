3933ee17b6781647b0d2f3e3bbee4959dd0dacab
christopher calascione & Jeff Austin <student@epicodus.com>
2015-09-21 15:39:52 -0700

    restaurant angular app

---
+++ app.js
@@ -0,0 +1,1 @@
+var restaurantList = angular.module('restaurantList', []);
---
+++ js/RestaurantController.js
@@ -0,0 +1,19 @@
+restaurantList.controller("RestaurantCtrl", function($scope){
+  $scope.restaurants = [];
+  // $scope.radio = '';
+  $scope.addRestaurant = function(){
+    $scope.restaurants.push({name: $scope.restaurantName,
+                             cuisine: $scope.cuisine,
+                             location: $scope.location,
+                             priceRange: $scope.priceRange});
+                             $scope.restaurantName = null;
+                             $scope.cuisine = null;
+                             $scope.location = null;
+                             $scope.priceRange = null;
+  };
+  $scope.deleteRestaurant = function(restaurant) {
+    var index = $scope.restaurants.indexOf(restaurant);
+    $scope.restaurants.splice(index, 1);
+  };
+
+});
